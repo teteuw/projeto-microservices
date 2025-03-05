4737f3a89f23c392fd079796202da8b7f7fe118d
Mateus <mateuaraujo01@gmail.com>
2025-03-05 17:37:55 -0300

    updating personcontroller e sevices

--- api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/controller/PersonController.java
+++ api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/controller/PersonController.java
@@ -1,26 +1,49 @@
 package br.com.teteu.api.gateway.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.teteu.api.gateway.model.Person;
 import br.com.teteu.api.gateway.services.PersonServices;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonServices service;
-    /* 
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
-    public Double sum(
-        @PathVariable(value = "numberOne")String numberOne,
-        @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
 
-        if(!MathUtils.isNumeric(numberOne) || !MathUtils.isNumeric(numberTwo)){
-                    throw new UnsupportedMathOperationException("Please set a valid number");
-                }
-            return SimpleMath.sum(numberOne, numberTwo);
-    */
+   
+    
+    @RequestMapping(value = "/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findPersonById(
+        @PathVariable(value = "id")String id) throws Exception{
+            return service.findPersonById(id);
+        }
 
+    @RequestMapping(value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+            return service.findAll();
+        }
+    @RequestMapping(method = RequestMethod.POST, 
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+        public Person createPerson(@RequestBody Person person){
+            return service.create(person);
+        }
+    @RequestMapping(method = RequestMethod.PUT,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+        public Person updatePerson(@RequestBody Person person){
+            return service.updatePerson(person);
+        }
+    @RequestMapping(method = RequestMethod.DELETE)
+        public void delete(@PathVariable(value = "id")String id){
+             service.deletePerson(id);
+        }
 }
--- api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/services/PersonServices.java
+++ api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/services/PersonServices.java
@@ -1,12 +1,59 @@
 package br.com.teteu.api.gateway.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
 
+import br.com.teteu.api.gateway.model.Person;
+
 @Service
 public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll(){
+        logger.info("finding persons");
+        List<Person> persons = new ArrayList<>();
+        for(int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+            }
+        return persons;
+    }
+    public Person create (Person person){
+        logger.info("creating one person");
+        return person;
+    }
+            
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.getAndIncrement());
+        person.setFirstName("Peron Name" + i);
+        person.setLastName("Last Name" + i);
+        person.setAdress("Uberlândia");
+        person.setGender("Male");
+        return person;
+    }
+            
+    public Person findPersonById(String id){
+        logger.info("finding a person");
+        Person person = new Person();
+        person.setId(counter.getAndIncrement());
+        person.setFirstName("Leandro");
+        person.setLastName("Costa");
+        person.setAdress("Uberlândia");
+        person.setGender("Male");
+        return person;
+    }
+    public Person updatePerson(Person person){
+        logger.info("updating one person");
+        return person;
+    }
+    public void deletePerson(String id){
+        logger.info("deleting one person");
+    }
+
 }
