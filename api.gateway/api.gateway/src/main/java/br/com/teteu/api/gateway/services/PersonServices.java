d93e8a923d7033db29e6e52989cdc38a7a771e37
Mateus <mateuaraujo01@gmail.com>
2025-03-06 14:23:30 -0300

    Mudando service e apliccation properties

--- api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/services/PersonServices.java
+++ api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/services/PersonServices.java
@@ -5,55 +5,59 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.teteu.api.gateway.exceptions.ResourceNotFoundException;
 import br.com.teteu.api.gateway.model.Person;
+import br.com.teteu.api.gateway.repository.PersonRepository;
 
 @Service
 public class PersonServices {
-    private final AtomicLong counter = new AtomicLong();
+
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
 
     public List<Person> findAll(){
         logger.info("finding persons");
-        List<Person> persons = new ArrayList<>();
-        for(int i = 0; i < 8; i++){
-            Person person = mockPerson(i);
-            persons.add(person);
-            }
-        return persons;
+    
+        return repository.findAll();
     }
     public Person create (Person person){
         logger.info("creating one person");
-        return person;
+        return repository.save(person);
     }
             
-    private Person mockPerson(int i) {
-        Person person = new Person();
-        person.setId(counter.getAndIncrement());
-        person.setFirstName("Peron Name" + i);
-        person.setLastName("Last Name" + i);
-        person.setAdress("Uberlândia");
-        person.setGender("Male");
-        return person;
-    }
-            
-    public Person findPersonById(String id){
+    public Person findPersonById(Long id){
         logger.info("finding a person");
-        Person person = new Person();
-        person.setId(counter.getAndIncrement());
-        person.setFirstName("Leandro");
-        person.setLastName("Costa");
-        person.setAdress("Uberlândia");
-        person.setGender("Male");
-        return person;
+       
+        return repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
     }
     public Person updatePerson(Person person){
+
         logger.info("updating one person");
-        return person;
+
+        var entity = repository.findById(person.getId())
+            .orElseThrow(() -> new ResourceNotFoundException("No records foud for this id"));
+        
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAdress(person.getAdress());
+        entity.setGender(person.getAdress());
+
+        return repository.save(person);
     }
-    public void deletePerson(String id){
+    public void deletePerson(Long id){
         logger.info("deleting one person");
+
+        repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+
+        repository.deleteById(id);;
+
     }
 
 }
