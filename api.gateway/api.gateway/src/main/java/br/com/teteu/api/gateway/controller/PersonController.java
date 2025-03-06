c3ebbe84f3c9d682bc89f2952f827c1a80164790
Mateus <mateuaraujo01@gmail.com>
2025-03-06 14:29:32 -0300

    refatorando o delete

--- api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/controller/PersonController.java
+++ api.gateway/api.gateway/src/main/java/br/com/teteu/api/gateway/controller/PersonController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +48,8 @@ public class PersonController {
             return service.updatePerson(person);
         }
     @DeleteMapping
-        public void delete(@PathVariable(value = "id")Long id){
+        public ResponseEntity<?> delete(@PathVariable(value = "id")Long id){
              service.deletePerson(id);
+             return ResponseEntity.noContent().build();
         }
 }
