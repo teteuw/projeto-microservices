6e9e2ee9878adae2510448296bada45442330075
Mateus <mateuaraujo01@gmail.com>
2025-03-10 07:41:38 -0300

    criando cambio service e comentando por enquanto a depdendencia de client config

---
+++ cambio-service/src/main/java/br/com/teteu/cambio_service/CambioServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.teteu.cambio_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambioServiceApplication.class, args);
+	}
+
+}
---
+++ cambio-service/src/test/java/br/com/teteu/cambio_service/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teteu.cambio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
