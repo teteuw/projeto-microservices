cb4f63fc51054aa14ae443fa4fcbd4aa48a9aacb
Mateus <mateuaraujo01@gmail.com>
2025-03-12 14:48:51 -0300

    criando book-service

---
+++ book-service/src/main/java/br/com/tteu/book_service/controller/BookController.java
@@ -0,0 +1,28 @@
+package br.com.tteu.book_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Date;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tteu.book_service.model.Book;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+    
+    @Autowired
+    Environment environment;
+
+    @GetMapping(value = "/{id}/{currency}")
+    public Book findBook(
+        @PathVariable("id") Long id,
+        @PathVariable("currency") String currency){
+            var port = environment.getProperty("local.server.port");
+            return new Book(1L, "Nigel", "Docker", new Date(), Double.valueOf(14.0), "currency", port);
+    }
+    
+}
