6811da0e0bc328769174aad82dceeb9d04205dcc
Mateus <mateuaraujo01@gmail.com>
2025-03-13 13:34:36 -0300

    mudando as aplications

--- naming-server/src/main/java/br/com/teteu/naming_server/NamingServerApplication.java
+++ naming-server/src/main/java/br/com/teteu/naming_server/NamingServerApplication.java
@@ -2,8 +2,9 @@ package br.com.teteu.naming_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
+@EnableEurekaServer
 public class NamingServerApplication {
 
 	public static void main(String[] args) {
